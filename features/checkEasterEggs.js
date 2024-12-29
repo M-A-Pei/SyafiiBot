63fa528f2bb23697c607d362fb3723a646098f0f
Muhammad Abdullah Syafii <syafii2006@gmail.com>
2024-12-29 14:49:59 +0700

    working easter eggs

--- features/checkEasterEggs.js
+++ features/checkEasterEggs.js
@@ -1,7 +1,7 @@
 const easterEggs = require("../data/easterEggs");
 
 async function checkEasterEggs(msg, sock, db) {
-    //if(msg.key.fromMe && msg?.messageStubParameters) return;
+    //if(msg.key.fromMe || msg?.messageStubParameters) return;
 
     const from = msg.key.remoteJid; // Sender's ID
     let text = ""
@@ -17,19 +17,49 @@ async function checkEasterEggs(msg, sock, db) {
     console.log("text from easter egg ", text)
     
     const easterEggPattern = new RegExp(easterEggs.join('|'), 'i'); // 'i' for case-insensitive match
-    if (easterEggPattern.test(text)) {
-        const matchedEasterEgg = easterEggs.find(word => text.includes(word));
-        if (matchedEasterEgg) {
-            try {
-                await sock.sendMessage(from, { text: "Easter Egg Found: " + matchedEasterEgg });
-                console.log("Easter Egg Found: " + matchedEasterEgg);
-            } catch (error) {
-                console.error("Error sending message:", error);
-            }
-        }
+    if (!easterEggPattern.test(text)) return;
+    const matchedEasterEgg = easterEggs.find(word => text.toLowerCase().includes(word.toLowerCase()));
+
+    if (!matchedEasterEgg) return;
+    const getEgg = () => {
+        return new Promise((resolve, reject) =>{
+            console.log(matchedEasterEgg)
+            db.get(`
+                SELECT * FROM easterEggs WHERE easterEggText = '${matchedEasterEgg}' COLLATE NOCASE AND found = FALSE;`, (err, row) => {
+                if (err) {
+                    console.error("Error reading table:", err.message);
+                    reject(err);
+                } else {
+                    console.log("Table read successfully.");
+                    resolve(row);
+                }
+            });
+        })
     }
 
-    
+    const dbEasterEgg = await getEgg()
+    if(!dbEasterEgg){
+        console.log("easter egg is already claimed!");
+        return;
+    }
+
+    db.run(`
+        UPDATE easterEggs SET found = TRUE, finder = '${msg.pushName}' 
+        WHERE easterEggText = '${matchedEasterEgg}' COLLATE NOCASE AND found = FALSE;
+        `, (err) => {
+        if (err) {
+            console.error("Error updating data:", err.message);
+        } else {
+            console.log("updated easter egg successfully.");
+        }
+    });
+
+    try {
+        await sock.sendMessage(from, { text: `Easter Egg *[${matchedEasterEgg}]* Found! by ${msg.pushName}`});
+        console.log("Easter Egg Found: " + matchedEasterEgg);
+    } catch (error) {
+        console.error("Error sending message:", error);
+    }
 }
 
 module.exports = checkEasterEggs
