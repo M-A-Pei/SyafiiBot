f445a280a39db4afbce902c5287bb2ac834fe47c
M Abdullah Syafii <115166418+M-A-Pei@users.noreply.github.com>
2024-12-30 20:44:58 +0700

    Update sendDailyPoll.js
    more time zone shenanigans

--- features/sendDailyPoll.js
+++ features/sendDailyPoll.js
@@ -20,7 +20,7 @@ async function sendDailyPoll(sock, db) {
         })
     }
 
-    if(hour >= 15 && !DailyPollSent){
+    if(hour >= 13 && !DailyPollSent){
         const dayCounter = await getDayCounter();
         const pollOptions = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
         
@@ -47,7 +47,7 @@ async function sendDailyPoll(sock, db) {
         console.log("dayCounter: " + dayCounter);
     }
 
-    if(hour < 15){
+    if(hour < 13){
         DailyPollSent = false;
     }
 }
